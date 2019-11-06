ba80711e5651cdc40e7ee31708835e9731df06b5
vagrant <vagrant@vagrant.vm>
2019-11-06 06:41:22 +0000

    add bootstrap script and fix base.html inheritance

--- mood_tracker.py
+++ mood_tracker.py
@@ -27,6 +27,11 @@ def home():
     return render_template('homepage.html', posts=posts)
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
